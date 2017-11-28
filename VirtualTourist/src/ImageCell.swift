c114a71ebd8f169ab0d6b96e6406df873bd592dc
jayjac <jy_jac@msn.com>
2017-11-28 13:38:23 +0100

    Moved ImageCell file

---
+++ VirtualTourist/src/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  VirtualTourist
+//
+//  Created by Jean-Yves Jacaria on 18/11/2017.
+//  Copyright © 2017 Jean-Yves Jacaria. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
